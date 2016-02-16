e9f7707c571859c7396808f8807879c6823970c6
Shockk <shokku.ra@gmail.com>
2016-02-16 11:42:05 +0000

    main: implement lab1 example

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
+#include <iostream>
 #include <gecode/driver.hh>
 #include <lab1.hpp>
 
 int main(int argc, char **argv) {
+	gameai::gecode::lab1 l1;
+	Gecode::DFS<decltype(l1)> e(&l1);
+	if(decltype(l1) *s = e.next()) {
+		s->print(std::cout);
+	}
 	return 0;
 }
