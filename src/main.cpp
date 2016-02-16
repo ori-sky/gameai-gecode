ad516ed029733f35207492f4597e4110a171bd68
Shockk <shokku.ra@gmail.com>
2016-02-16 12:17:02 +0000

    main: clean up copies of objects

--- src/main.cpp
+++ src/main.cpp
@@ -9,18 +9,21 @@ int main(int argc, char **argv) {
 	Gecode::DFS<decltype(l1)> e1(&l1);
 	while(decltype(l1) *s = e1.next()) {
 		s->print(std::cout);
+		delete s;
 	}
 
 	gameai::gecode::lab2 l2;
 	Gecode::DFS<decltype(l2)> e2(&l2);
 	while(decltype(l2) *s = e2.next()) {
 		s->print(std::cout);
+		delete s;
 	}
 
 	gameai::gecode::lab3 l3;
 	Gecode::DFS<decltype(l3)> e3(&l3);
 	while(decltype(l3) *s = e3.next()) {
 		s->print(std::cout);
+		delete s;
 	}
 
 	return 0;
