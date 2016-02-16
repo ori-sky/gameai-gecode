faefda2bc7cb678e4d28cefaf72579a0629f73d2
Shockk <shokku.ra@gmail.com>
2016-02-16 12:04:03 +0000

    main: replace `if` with `while`

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 int main(int argc, char **argv) {
 	gameai::gecode::lab1 l1;
 	Gecode::DFS<decltype(l1)> e(&l1);
-	if(decltype(l1) *s = e.next()) {
+	while(decltype(l1) *s = e.next()) {
 		s->print(std::cout);
 	}
 	return 0;
